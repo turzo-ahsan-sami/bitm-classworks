f38d21b5d215087e8d6623f14437b4d7fe7d8d67
Turzo Ahsan Sami <turzoahsansami@gmail.com>
2019-09-09 06:58:45 +0600

    assignment 1

---
+++ ConsoleUserIO/ConsoleUserIO/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleUserIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserDetails user = new UserDetails();
+
+            // Input
+
+            Console.WriteLine("Input Name : ");
+            user.User_name = Console.ReadLine();
+            Console.WriteLine("Input Age : ");
+            user.User_age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input Phone : ");
+            user.User_phone = Console.ReadLine();
+            Console.WriteLine("Input Address : ");
+            user.User_address = Console.ReadLine();
+
+            // Output
+
+            Console.WriteLine("Name : " + user.User_name);
+            Console.WriteLine("Age : " + user.User_age);
+            Console.WriteLine("Phone : " + user.User_phone);
+            Console.WriteLine("Address : " + user.User_address);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleUserIO/ConsoleUserIO/UserDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUserIO
+{
+    class UserDetails
+    {
+
+        private string user_name;
+        private int user_age;
+        private string user_phone;
+        string user_address;
+
+        public string User_name { get => user_name; set => user_name = value; }
+        public int User_age { get => user_age; set => user_age = value; }
+        public string User_phone { get => user_phone; set => user_phone = value; }
+        public string User_address { get => user_address; set => user_address = value; }
+
+    }
+}
